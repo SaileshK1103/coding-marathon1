2b1e471527077348c704d3ce4acef8454f7da13a
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2025-11-14 11:26:20 +0200

    Add email input and log the value

--- src/components/SignupPage/SignupPage.jsx
+++ src/components/SignupPage/SignupPage.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import { useState } from 'react';
 
-function SignupPage() {
-  return (
-    <div>
-      SignupPage
-    </div>
-  )
+function SignUpPage() {
+
+const [email, setEmail] = useState("");
+
+
+const handleInputChange = (event) => {
+  setEmail(event.target.name);
+  console.log(event.target.name);
+
 }
 
-export default SignupPage
- 
+    return(
+        <section>
+          <h1>Sign Up</h1>
+            <h4>Create a free account now</h4>
+              <div>
+                <p>Email:</p>
+                  <input
+                    type="text"
+                    name="email"
+                    placeholder="john.doe@fake.com"
+                    value={email}
+                    onChange={handleInputChange}
+                  ></input>
+              </div>
+        </section>
+
+    );
+}
+
+export default SignUpPage;
